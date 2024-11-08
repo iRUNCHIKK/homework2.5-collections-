93f546ebc0480d4f5589f7f44c1966bf59604755
iRUNCHIKK <btkv_irina@icloud.com>
2024-11-08 21:52:20 +0500

    Initial commit

---
+++ src/main/java/ru/skypro/springcollections/SpringCollectionsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.springcollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCollectionsApplication.class, args);
+	}
+
+}
